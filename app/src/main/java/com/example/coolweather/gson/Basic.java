548737a45f609e31d1a1781f588b64454becc9d2
qiaojiange <1967254382@qq.com>
2017-03-26 12:18:46 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -0,0 +1,26 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by qiaojiange on 2017/3/24.
+ */
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+
+    public Update update;
+
+    public class Update{
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/gson/Suggestion.java
@@ -0,0 +1,39 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by qiaojiange on 2017/3/24.
+ */
+
+public class Suggestion {
+
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public CarWash carWash;
+
+    @SerializedName("sport")
+    public Sport sport;
+
+    public class Comfort {
+
+        @SerializedName("txt")
+        public String info;
+    }
+
+
+    public class CarWash{
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Sport{
+
+        @SerializedName("txt")
+        public String info;
+    }
+
+
+}
